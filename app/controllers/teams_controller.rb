2ecfd942c0addd0701f394c6be0d1d64cf635237
anbranin <keichelman@gmail.com>
2018-02-11 09:01:42 -0500

    be able to add teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,6 +5,17 @@ class TeamsController < ApplicationController
   end
 
   def create
-    # params should include a team name
+    team = Team.new(name: params[:name])
+    if team.save
+      flash[:success] = 'Team Created Successfully'
+      redirect_to game_url(@game) and return
+    else
+      flash[:errors] = team.errors.full_messages
+      redirect_to action: :new and return
+    end
+  end
+
+  def new
+    @divisions = Division.all
   end
 end
