69c9a82a9268211b01ce0798e3f64cd0eafef9a0
anbranin <keichelman@gmail.com>
2018-02-11 08:59:34 -0500

    nested routes for division_teams.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  root 'teams#index'
+  root 'games#index'
 
-  resources :teams, :division_teams
+  resources :games do
+    member do
+      get :girls_pool
+      get :boys_pool
+      get :girls_spirit
+      get :boys_spirit
+      get :girls_bracket
+      get :boys_bracket
+    end
+    resources :division_teams
+  end
+
+  resources :teams
+  resources :divisions
 end
