6dccfe4540411300cfad3a7a6c799a820f1d07da
Anbranin <keichelman@gmail.com>
2020-02-21 10:24:52 -0500

    this makes sense

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
   has_many :division_teams
   has_many :divisions, through: :division_teams
+  has_many :teams, through: :division_teams
 
   validates :name, uniqueness: true, presence: true
-
 end
