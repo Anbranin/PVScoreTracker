fd23a67198ab85cba0a3a051ae1287010f061292
Anbranin <keichelman@gmail.com>
2020-02-21 10:24:44 -0500

    commonsense validation for year

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  validates :year, uniqueness: true, presence: true
+  validates :year, uniqueness: true, presence: true, length: { is: 4 }
 
   has_many :division_teams
 
