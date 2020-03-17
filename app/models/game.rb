ed66de2c13c4cacfa1eab118e786b5e83ed2b8eb
Anbranin <keichelman@gmail.com>
2020-03-17 09:50:42 -0400

    use different join model. simpler.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   validates :year, uniqueness: true, presence: true, length: { is: 4 }
 
-  has_many :division_teams
+  has_many :games_teams
+  has_many :teams, through: :games_teams
 
 end
